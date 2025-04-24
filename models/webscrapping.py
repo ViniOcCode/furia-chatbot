e67ea613ca3b5fa268e970f2a7bd4537f3acdbe3
vinicius <viniciusoctavio@hotmail.com>
2025-04-24 15:54:05 -0300

    feat: webscraping working gud

--- models/webscrapping.py
+++ models/webscrapping.py
@@ -1,24 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.hltv.org/team/8297/furia#tab-matchesBox' 
-URL_TESTE = 'https://www.hltv.org/team/9565/vitality#tab-matchesBox'
+DOMAIN = 'https://www.hltv.org/'
 HEADERS = {'User-Agent': 'Mozzila/5.0'}
 
-# Uses the url 
-def fetch_html(url, headers):
-    response = requests.get(url, headers=headers).text
-    return  BeautifulSoup(response, 'html.parser')
+def main():
+    print(get_soon_matches(DOMAIN, HEADERS))
+    print(get_players(DOMAIN, HEADERS))
 
-# extensive use of get_text
-def extract_text(row):
-    return row.get_text(strip=True) if row else None
+# Uses the url 
+def get_soon_matches(domain, headers):
+    url = f'{domain}team/8297/furia#tab-matchesBox' 
+    urlt = f'{domain}team/12774/liquid#tab-matchesBox'
+    response = requests.get(urlt, headers=headers).text
+    soup = BeautifulSoup(response, 'html.parser')
+    return soon_matches(soup) 
+
+def get_players(domain, headers):
+    url = f'{domain}team/8297/furia#tab-rosterBox' 
+    response = requests.get(url, headers=headers).text
+    soup = BeautifulSoup(response, 'html.parser')
+    return players(soup) 
+
 
 # get the matches
-def parse_matches(soup):
-
-    # check for not schedule
-    if soup.find('div', class_='empty-state'):
+def soon_matches(soup):
+    # check for not schedule going inside the main matchesbox, then searching only
+    # empty-states there
+    # probably a team with no recent results would broke this, but idk
+    if soup.find('div', id='matchesBox').find('div', class_='empty-state'):
         return 'As panteras não têm jogo marcado :('
 
     table = soup.find('table', class_='match-table')
@@ -28,8 +38,11 @@ def parse_matches(soup):
     event = date = team = None 
     for row in rows:
         # Get event name
-        event= extract_text(row.find('a', class_='a-reset')) or event
+        livestream = row.find('a', class_='matchpage-button')
+        if livestream != None:
+            watch = livestream['href']
 
+        event = extract_text(row.find('a', class_='a-reset')) or event
 
         # Get matches date
         date = extract_text(row.find('td', class_='date-cell')) or event
@@ -47,16 +60,44 @@ def parse_matches(soup):
             match = {
                 'Event': event,
                 'Date': date,
-                'Enemy': team
+                'Enemy': team,
+                'livestreams': watch
             }
             matches.append(match)
             event = date = team = None 
     return matches
 
-# return the dict to user
-def find_matches(url, headers):
-    soup = fetch_html(url, headers)
-    return parse_matches(soup)
+def players(soup):
+    elenco = []
 
-print(find_matches(URL_TESTE, HEADERS))
- 
+    # Get coach
+    coach_row = soup.find('table', class_='coach-table').find('div', class_='text-ellipsis')
+    coach = extract_text(coach_row)
+    player = {
+        'coach': coach
+    }
+    elenco.append(player)
+
+    table = soup.find('table', class_='players-table')
+    rows = table.find_all('tr')
+
+    for row in rows:
+        
+        players = extract_text(row.find('div', class_='text-ellipsis'))
+        if row.find('div', class_='player-benched'):
+            players = f'{players} - (Reserva)'
+
+        player = {
+            'player': players
+        }
+        if players != None:
+            elenco.append(player)
+    
+    return elenco
+
+# extensive use of get_text
+def extract_text(row):
+    return row.get_text(strip=True) if row else None
+
+if __name__ == '__main__':
+    main()
